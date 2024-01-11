c21a44b5cb6b53c5a0306baedd9e862033adafb4
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-11 17:45:42 +0530

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-
 import './App.css';
 import Navbar from './Navbar';
-import Home  from './Home';
+import Home from './Home';
 
 function App() {
-
   return (
     <div className="App">
       <Navbar />
-      <div className='content'>
-        <Home/>
+      <div className="content">
+        <Home />
       </div>
-
     </div>
   );
 }
-
 export default App;
