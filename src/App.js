617977ee782bee9f74241255b71aeb00bdfe9056
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-11 18:09:43 +0530

    Click events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './index.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick= () =>
+{
+    console.log('hello');
+}
+
+const handleClickAgain = (name) =>{
+    console.log('hello' +name);
+}
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={()=>{
+                handleClickAgain(' mario')
+            }}>Click me again</button>
         </div>
     );
 
