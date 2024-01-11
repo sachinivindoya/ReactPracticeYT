7fd2120f8b0edc2e7e78218d1ab1ba2802936447
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-11 11:20:58 +0530

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home  from './Home';
 
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
-
-        <a href={link}>Google</a>
+        <Home/>
       </div>
+
     </div>
   );
 }
