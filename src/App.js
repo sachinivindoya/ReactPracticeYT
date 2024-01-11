ced19de321fe3daeac9f1cc195fbaa6654eca7b6
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-11 11:01:03 +0530

    Element Tags

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 function App() {
   const title = 'welcome to the new blog';
   const likes = 50;
+  const link = "http://www.google.com";
 
   return (
     <div className="App">
       <div className='content'>
         <h1>{title}</h1>
         <p>liked {likes} times</p>
+
+        <a href={link}>Google</a>
       </div>
     </div>
   );
